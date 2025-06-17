123927f53282aaec84add9de853f4e160cdbb27b
arielmusa <arielmusainfo@gmail.com>
2025-06-17 17:32:02 +0200

    feat(HomePage.jsx): render movie list using bootstrap card

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,36 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { NavLink } from "react-router";
 
 const apiUrl = "http://localhost:3000";
 
 export default function HomePage() {
+  const [movies, setMovies] = useState([]);
   useEffect(() => {
-    axios.get(`${apiUrl}/movies`).then((res) => console.log(res));
+    axios.get(`${apiUrl}/movies`).then((res) => setMovies(res.data));
   }, []);
 
-  return <h1>hello world</h1>;
+  return (
+    <>
+      <div className="container py-5">
+        <div className="row g-5">
+          {movies.map((movie) => (
+            <div className="col-md-3">
+              <div className="card">
+                <div className="card-body">
+                  <p className="card-text"> {movie.title}</p>
+                  <NavLink
+                    to={`movies/${movie.id}`}
+                    className="btn btn-primary"
+                  >
+                    Go somewhere
+                  </NavLink>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
