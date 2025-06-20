2c64d6f2cac6c3be1f5b9b51a2bc36626012f145
arielmusa <arielmusainfo@gmail.com>
2025-06-20 15:23:51 +0200

    feat(ReviewForm.jsx): add functions onChange and onSubmit and make AJAX call to store data in db

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -1,28 +1,72 @@
+import axios from "axios";
+import { useState } from "react";
+import { useParams } from "react-router";
+
+const initialDataForm = {
+  name: "",
+  vote: "",
+  text: "",
+};
+
+const apiUrl = "http://localhost:3000";
+
 export default function ReviewForm() {
+  const [form, setForm] = useState(initialDataForm);
+  const { id } = useParams();
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post(`${apiUrl}/movies/${id}/review`, form);
+    setForm(initialDataForm);
+  };
+
   return (
     <div className="container">
-      <form className="row gy-4 needs-validation">
+      <form className="row gy-4 needs-validation" onSubmit={handleSubmit}>
         <div className="col">
           <label className="form-label" htmlFor="name">
             Name
           </label>
-          <input className="form-control" type="text" name="name" required />
+          <input
+            onChange={handleChange}
+            value={form.name}
+            className="form-control"
+            type="text"
+            name="name"
+            required
+          />
         </div>
         <div className="col">
           <label className="form-label" htmlFor="vote">
             Vote
           </label>
-          <input className="form-control" type="number" name="vote" required />
+          <input
+            onChange={handleChange}
+            value={form.vote}
+            className="form-control"
+            type="number"
+            name="vote"
+            required
+          />
         </div>
         <div className="col-12">
           <label htmlFor="text" className="form-label">
             Your feedback
           </label>
-          <textarea className="form-control" name="text"></textarea>
+          <textarea
+            onChange={handleChange}
+            value={form.text}
+            className="form-control"
+            name="text"
+          ></textarea>
         </div>
-        <div class="col-12">
-          <button type="submit" class="btn btn-primary">
-            Sign in
+        <div className="col-12">
+          <button type="submit" className="btn btn-primary">
+            Submit
           </button>
         </div>
       </form>
